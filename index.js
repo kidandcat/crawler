8d96140a7c776184c2c89dd1c4e0adb08658b183
jairo <kidandcat@gmail.com>
2017-11-13 00:05:26 +0100

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    }); // default is true
+    //const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await listeners(browser, page);
+    await page.goto('https://animeflv.net');
+
+
+    const el = await page.evaluate(() => {
+        return document.querySelector('.ListEpisodios');
+    });
+    console.log('EL', el);
+
+
+    await browser.close();
+})();
+
+
+async function listeners(browser, page) {
+    browser.on('targetcreated', async target => {
+        const pg = await target.page();
+        if (pg) {
+            pg.close();
+        }
+    });
+
+    page.on('console', msg => {
+        console.log('PAGE LOG: ', msg);
+    });
+}
